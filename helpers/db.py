20fd6726a0f68b606592f4363374500f87c04825
SajjadLuqman <sajjadlogic@live.com>
2026-05-08 09:58:31 +0400

    Fixed Version Issues

--- helpers/db.py
+++ helpers/db.py
@@ -3,8 +3,8 @@ import pyodbc
 def get_connection():
     conn = pyodbc.connect(
         "DRIVER={ODBC Driver 17 for SQL Server};"
-        "SERVER=SAJJAD;"
-        "DATABASE=ACCOUNT_TPS;"
+        "SERVER=localhost;"
+        "DATABASE=ACCOUNT_SampleTest;"
         "Trusted_Connection=yes;"
         "TrustServerCertificate=yes;"
     )
